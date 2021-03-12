a30e283305170c66ee71b76a25df68594ead587d
Attila Domokos <adomokos@gmail.com>
2021-03-12 17:01:19 -0600

    Refactor the code to use map and sum

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -3,13 +3,4 @@ def add(numbers: str) -> int:
     if numbers == "":
         return 0
 
-    result = 0
-
-    if "," in numbers:
-        num_list = numbers.split(",")
-        for item in num_list:
-            result = result + int(item)
-    else:
-        result = int(numbers)
-
-    return result
+    return sum(map(int, numbers.split(",")))
